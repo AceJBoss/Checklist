c3100c9a52bca7664b3b4e6ea694e205408cc5b4
JBoss <abolade.akintomiwa@gmail.com>
2020-07-15 23:19:11 +0100

    Quick fix

--- src/main/java/com/logistics/checklist/services/AdminService.java
+++ src/main/java/com/logistics/checklist/services/AdminService.java
@@ -193,7 +193,6 @@ public class AdminService {
 		servicesRepository.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-
 	// create new package
 	public ResponseEntity<ServicePackage> createPackage(final ServicePackage packageData){
 		
